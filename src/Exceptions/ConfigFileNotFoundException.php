7e1579c5968953a7f640fe1aa30e22c2a2458aea
Dmytro <dima.berezniy.13@gmail.com>
2020-07-28 17:58:00 +0300

    feat: add exceptions

---
+++ src/Exceptions/ConfigFileNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bereznii\TeamsConnector\Exceptions;
+
+/**
+ * Class ConfigFileNotFoundException
+ *
+ * @author Dmytro Bereznii <bereznii.d@gmail.com>
+ */
+class ConfigFileNotFoundException extends Exception
+{
+
+}
---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bereznii\TeamsConnector\Exceptions;
+
+use Exception as CoreException;
+
+/**
+ * Class Exception
+ *
+ * @author Dmytro Bereznii <bereznii.d@gmail.com>
+ */
+class Exception extends CoreException
+{
+
+}
